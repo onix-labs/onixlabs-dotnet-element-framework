cd8d1d16bebf54b683a9b9970a39a7340b50cd1c
matthew <matthew.layton@live.co.uk>
2026-05-09 22:53:58 +0100

    Replaced proprietary license with MIT license across all files.

--- OnixLabs.ElementFramework.Neo4j/Neo4jGraphTransaction.cs
+++ OnixLabs.ElementFramework.Neo4j/Neo4jGraphTransaction.cs
@@ -1,42 +1,24 @@
-// All Rights Reserved License
-//
-// 1. Grant of License
-// Subject to the terms and conditions of this License, ONIXLabs ("Licensor") hereby grants to you a limited, non-exclusive, non-transferable, non-sublicensable license to use the Software for commercial, private, and paid purposes. This license does not include any rights to modify, distribute, or create derivative works of the Software.
-//
-// 2. Permitted Uses
-// You are permitted to:
-//  - Use the Software for commercial purposes.
-//  - Use the Software for private purposes.
-//  - Use the Software for paid purposes.
-//  - Exercise any patent rights associated with the Software, solely in connection with your use of the Software as permitted under this License.
-//
-// 3. Restrictions
-// You are not permitted to:
-//  - Modify, alter, or create any derivative works of the Software.
-//  - Distribute, sublicense, lease, rent, or otherwise transfer the Software to any third party.
-//  - Use the Software without obtaining a proper license for paid use.
-//  - Use the Software in any way that infringes upon the trademarks, service marks, or trade names of the Licensor.
-//  - Use the Software in any manner that could cause it to be considered open-source software or otherwise subject to an open-source license.
-//
-// 4. No Free Use
-// This license does not permit any free use of the Software. Any use of the Software without a paid license is strictly prohibited.
-//
-// 5. No Liability
-// To the maximum extent permitted by applicable law, the Software is provided "as is" and "as available" without warranty of any kind, express or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the Licensor be liable for any damages whatsoever arising out of the use of or inability to use the Software, even if the Licensor has been advised of the possibility of such damages.
-//
-// 6. No Warranty
-// The Licensor makes no warranty that the Software will meet your requirements, be uninterrupted, secure, or error-free. The Licensor disclaims all warranties with respect to the Software, whether express or implied, including but not limited to any warranties of merchantability, fitness for a particular purpose, and non-infringement.
-//
-// 7. Termination
-// This license is effective until terminated. Your rights under this license will terminate automatically without notice if you fail to comply with any term of this license. Upon termination, you must immediately cease all use of the Software and destroy all copies of the Software in your possession or control.
-//
-// 8. Governing Law
-// This license will be governed by and construed in accordance with the laws of [Your Jurisdiction], without regard to its conflict of laws principles.
-//
-// 9. Entire Agreement
-// This license constitutes the entire agreement between you and the Licensor concerning the Software and supersedes all prior or contemporaneous communications, agreements, or understandings, whether oral or written, concerning the subject matter hereof.
-//
-// By using the Software, you acknowledge that you have read and understood this license and agree to be bound by its terms and conditions.
+// MIT License
+
+// Copyright (c) 2020 ONIXLabs
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 using Neo4j.Driver;
 
